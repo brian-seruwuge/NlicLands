7c3b28ff52ae4ad336a0f63f1f511491a568bec7
Brian Sseruwugge <brianfast1000@gmail.com>
2020-05-09 09:42:43 +0300

    testing deploy to git hub pages

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+// import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div >
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
